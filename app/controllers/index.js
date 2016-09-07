6b3b6cd5140962ddd7f7565bce2d38c153851f00
cesaring <cesaring@hotmail.com>
2016-09-07 02:51:50 -0400

    updating latest change

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -31,10 +31,16 @@ export default Ember.Controller.extend({
      const email = this.get('emailAddress');
      
      const newInvitation = this.store.createRecord('invitation', {email:email});
-     newInvitation.save();   
      
-      this.set('responseMessage', `Thank you! We've justed saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+     newInvitation.save().then((response) =>{
+         console.log('Email address is saved');
+         
+         this.set('responseMessage', `Thank you! We've justed saved your email address with id: ${response.get('id')}`);
+         this.set('emailAddress', '');
+         
+     });
+     
+
     }
   }
 
