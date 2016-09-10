cee37f330a4434a87a4f556b5156b6135adecf90
Cesar Gonzalez <Cesar Gonzalez>
2016-09-10 13:27:20 -0400

    fixed type on new.js: transitionTo('libraries')...

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -8,7 +8,7 @@ export default Ember.Route.extend({
    
    actions: { 
     saveLibrary(newLibrary) {
-        newLibrary.save().then(() => this.transitionTo('libraryies'));
+        newLibrary.save().then(() => this.transitionTo('libraries'));
     },
     
     willTransition() {
