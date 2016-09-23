24ed18d17782081e2ba309105f2f58020f1880f2
cesaring <cesaring@hotmail.com>
2016-09-23 02:03:33 -0400

    meged the new/edit library form for each template

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -5,6 +5,18 @@ export default Ember.Route.extend({
     model() {
         return this.store.createRecord('library');
     },
+    
+    setupController: function (controller, model) {
+        this._super(controller, model);
+        
+        controller.set('title', 'Create a new library');
+        controller.set('buttonLabel', 'Create');
+    },
+    
+    //I think if this wasn't here it would go to /libraries/new
+    renderTemplate() {
+        this.render('libraries/form');
+    },
    
    actions: { 
     saveLibrary(newLibrary) {
@@ -14,7 +26,12 @@ export default Ember.Route.extend({
     willTransition() {
         //rollbackAttributes() removes the record from the store
         //if the model 'isNew'
-        this.controller.get('model').rollbackAttributes();
-    }
-    }
+        let model = this.controller.get('model);')
+        
+        if (model.get('isNew')) {
+            model.destroyRecord();
+        } //if
+    }//willTransition
+   }//actions
+   
 });
