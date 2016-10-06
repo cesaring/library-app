195aa4a3560b071e66ad8de3016caf91eab3b059
cesaring <cesaring@hotmail.com>
2016-10-06 05:27:06 -0400

    Added Edit Author Name feature...

--- app/models/author.js
+++ app/models/author.js
@@ -14,4 +14,5 @@ export default DS.Model.extend({
       return this;
   }
   
+  
 });
--- app/routes/authors.js
+++ app/routes/authors.js
@@ -3,5 +3,31 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('author');
+        
+    },
+    
+    actions:{
+        
+        saveAuthor(author) {
+            if (author.get('isNotValid')) {
+                return;
+            }
+            
+            author.set('isEditing', false);
+            author.save();
+            
+        },
+        
+        cancelAuthorEdit(author){
+            author.set('isEditing', false);
+            author.rollbackAttributes();
+            
+        },
+        
+        editAuthor(author) {
+            author.set('isEditing', true);
+            
+        }
+        
     }
 });
