1c0a921449f7d9f4d29f7285b5e9520d208a1d47
cesaring <cesaring@hotmail.com>
2016-10-08 02:02:13 -0400

    added Allow Editing of Title in Books route.

--- app/routes/books.js
+++ app/routes/books.js
@@ -5,7 +5,29 @@ export default Ember.Route.extend({
     
     model() {
         return this.store.findAll('book');
-    }   
+    }  ,
+    
+    actions: {
+        saveBook(book) {
+            if (book.get('isNotValid')) {
+                return;
+            }
+            else {
+                book.set('isEditing', false);
+                book.save();
+            }
+        },
+        
+        editBook(book) {
+            book.set('isEditing', true) ;
+        },
+        
+        cancelBookEdit(book) {
+            book.set('isEditing', false);
+            book.rollbackAttributes();
+        }
+        
+    } 
     
     
 });
